a9440ec6a20e22fce630c07f9315d69481fced92
Nahianether <nahianethr3@gmail.com>
2022-08-09 09:14:58 +0600

    first commit

---
+++ lib/pdf_invoice_api.dart
@@ -0,0 +1,394 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'file_handle_api.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+
+class PdfInvoiceApi {
+  static Future<File> generate() async {
+    final pdf = pw.Document();
+
+    final iconImage =
+        (await rootBundle.load('assets/ag_logo.png')).buffer.asUint8List();
+
+    final tableHeaders = [
+      'SL',
+      'ITEM',
+      'Qty',
+      'Price',
+      'Total',
+    ];
+
+    final tableData = [
+      [
+        '1',
+        'Coffee',
+        '7',
+        '\$ 5',
+        '\$ 35',
+      ],
+      [
+        '2',
+        'Blue Berries',
+        '5',
+        '\$ 10',
+        '\$ 50',
+      ],
+      [
+        '3',
+        'Water',
+        '1',
+        '\$ 3',
+        '\$ 3',
+      ],
+      [
+        '4',
+        'Apple',
+        '6',
+        '\$ 8',
+        '\$ 48',
+      ],
+      [
+        '5',
+        'Lunch',
+        '3',
+        '\$ 90',
+        '\$ 270',
+      ],
+      [
+        '6',
+        'Drinks',
+        '2',
+        '\$ 15',
+        '\$ 30',
+      ],
+      [
+        '7',
+        'Lemon',
+        '4',
+        '\$ 7',
+        '\$ 28',
+      ],
+    ];
+
+    pdf.addPage(
+      pw.MultiPage(
+        // header: (context) {
+        //   return pw.Text(
+        //     'Flutter Approach',
+        //     style: pw.TextStyle(
+        //       fontWeight: pw.FontWeight.bold,
+        //       fontSize: 15.0,
+        //     ),
+        //   );
+        // },
+        build: (context) {
+          return [
+            pw.Column(
+              mainAxisSize: pw.MainAxisSize.min,
+              mainAxisAlignment: pw.MainAxisAlignment.center,
+              crossAxisAlignment: pw.CrossAxisAlignment.start,
+              children: [
+                pw.Row(
+                  mainAxisAlignment: pw.MainAxisAlignment.center,
+                  children: [
+                    pw.Column(
+                      children: [
+                        pw.Image(
+                          pw.MemoryImage(iconImage),
+                          height: 72,
+                          width: 72,
+                        ),
+                        pw.Text(
+                          'Flutter Test Demo',
+                          style: const pw.TextStyle(
+                            fontSize: 15.0,
+                            color: PdfColors.grey700,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                pw.SizedBox(height: 1 * PdfPageFormat.mm),
+                pw.Divider(),
+                pw.SizedBox(height: 1 * PdfPageFormat.mm),
+                pw.Column(
+                  mainAxisSize: pw.MainAxisSize.min,
+                  mainAxisAlignment: pw.MainAxisAlignment.start,
+                  crossAxisAlignment: pw.CrossAxisAlignment.start,
+                  children: [
+                    pw.Text(
+                      'CONTACT INFO',
+                      style: pw.TextStyle(
+                        fontWeight: pw.FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    pw.Text(
+                      'Algorithm Generation Limited',
+                      style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                    ),
+                    pw.SizedBox(height: 1 * PdfPageFormat.mm),
+                    pw.Row(
+                      mainAxisAlignment: pw.MainAxisAlignment.center,
+                      children: [
+                        pw.Text(
+                          'Address: ',
+                          style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                        ),
+                        pw.Text(
+                          '1107/A, Baitul Aman Housing Society Limited, Ring Road, Shaymoli, 2nd Floor, Dhaka',
+                          maxLines: 5,
+                          overflow: pw.TextOverflow.clip,
+                        ),
+                      ],
+                    ),
+                    pw.SizedBox(height: 1 * PdfPageFormat.mm),
+                    pw.Row(
+                      mainAxisAlignment: pw.MainAxisAlignment.start,
+                      children: [
+                        pw.Text(
+                          'Email: ',
+                          style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                        ),
+                        pw.Text(
+                          'dev@adnanfoundation.com',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            pw.SizedBox(height: 1 * PdfPageFormat.mm),
+            pw.Divider(),
+            pw.SizedBox(height: 1 * PdfPageFormat.mm),
+            pw.Text(
+              'Dear John,\nLorem ipsum dolor sit amet consectetur adipisicing elit.',
+              textAlign: pw.TextAlign.justify,
+            ),
+            pw.SizedBox(height: 5 * PdfPageFormat.mm),
+
+            ///
+            /// PDF Table Create
+            ///
+            pw.Table.fromTextArray(
+              headers: tableHeaders,
+              data: tableData,
+              border: null,
+              headerStyle: pw.TextStyle(
+                fontWeight: pw.FontWeight.bold,
+              ),
+              headerDecoration: const pw.BoxDecoration(
+                color: PdfColors.grey300,
+              ),
+              cellHeight: 30.0,
+              cellAlignments: {
+                0: pw.Alignment.centerLeft,
+                1: pw.Alignment.centerLeft,
+                2: pw.Alignment.centerRight,
+                3: pw.Alignment.centerRight,
+                4: pw.Alignment.centerRight,
+              },
+            ),
+            pw.Divider(),
+            pw.Container(
+              alignment: pw.Alignment.centerRight,
+              child: pw.Row(
+                children: [
+                  //pw.Spacer(flex: 6),
+                  pw.Expanded(
+                    flex: 4,
+                    child: pw.Column(
+                      crossAxisAlignment: pw.CrossAxisAlignment.start,
+                      children: [
+                        pw.Row(
+                          children: [
+                            pw.Expanded(
+                              child: pw.Text(
+                                'Sub total:',
+                                style: pw.TextStyle(
+                                  fontWeight: pw.FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            pw.Text(
+                              '\$ 464',
+                              style: pw.TextStyle(
+                                fontWeight: pw.FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        pw.SizedBox(height: 1 * PdfPageFormat.mm),
+                        pw.Row(
+                          children: [
+                            pw.Expanded(
+                              child: pw.Text(
+                                'Discount:',
+                                style: pw.TextStyle(
+                                  fontWeight: pw.FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            pw.Text(
+                              '\$ 464',
+                              style: pw.TextStyle(
+                                fontWeight: pw.FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        pw.SizedBox(height: 1 * PdfPageFormat.mm),
+                        pw.Row(
+                          children: [
+                            pw.Expanded(
+                              child: pw.Text(
+                                'Gross total:',
+                                style: pw.TextStyle(
+                                  fontWeight: pw.FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            pw.Text(
+                              '\$ 464',
+                              style: pw.TextStyle(
+                                fontWeight: pw.FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        pw.SizedBox(height: 1 * PdfPageFormat.mm),
+                        pw.Row(
+                          children: [
+                            pw.Expanded(
+                              child: pw.Text(
+                                'Vat:',
+                                style: pw.TextStyle(
+                                  fontWeight: pw.FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            pw.Text(
+                              '\$ 90.48',
+                              style: pw.TextStyle(
+                                fontWeight: pw.FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        pw.SizedBox(height: 1 * PdfPageFormat.mm),
+                        pw.Row(
+                          children: [
+                            pw.Expanded(
+                              child: pw.Text(
+                                'Net total:',
+                                style: pw.TextStyle(
+                                  fontWeight: pw.FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            pw.Text(
+                              '\$ 464',
+                              style: pw.TextStyle(
+                                fontWeight: pw.FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        pw.SizedBox(height: 2 * PdfPageFormat.mm),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            pw.Container(
+              alignment: pw.Alignment.centerRight,
+              child: pw.Row(
+                children: [
+                  //pw.Spacer(flex: 6),
+                  pw.Expanded(
+                    flex: 4,
+                    child: pw.Column(
+                      crossAxisAlignment: pw.CrossAxisAlignment.start,
+                      children: [
+                        pw.SizedBox(height: 1 * PdfPageFormat.mm),
+                        pw.Row(
+                          children: [
+                            pw.Expanded(
+                              child: pw.Text(
+                                'CASH',
+                                style: pw.TextStyle(
+                                  fontWeight: pw.FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            pw.Text(
+                              '\$ 464',
+                              style: pw.TextStyle(
+                                fontWeight: pw.FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        pw.SizedBox(height: 1 * PdfPageFormat.mm),
+                        pw.Row(
+                          children: [
+                            pw.Expanded(
+                              child: pw.Text(
+                                'CARD',
+                                style: pw.TextStyle(
+                                  fontWeight: pw.FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            pw.Text(
+                              '\$ 464',
+                              style: pw.TextStyle(
+                                fontWeight: pw.FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        pw.SizedBox(height: 1 * PdfPageFormat.mm),
+                        pw.Row(
+                          children: [
+                            pw.Expanded(
+                              child: pw.Text(
+                                'Digital Wallet',
+                                style: pw.TextStyle(
+                                  fontWeight: pw.FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            pw.Text(
+                              '\$ 90.48',
+                              style: pw.TextStyle(
+                                fontWeight: pw.FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        pw.SizedBox(height: 5 * PdfPageFormat.mm),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            pw.SizedBox(height: 3 * PdfPageFormat.mm),
+            pw.Text(
+              'Dear John, Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia.',
+              textAlign: pw.TextAlign.justify,
+            ),
+          ];
+        },
+      ),
+    );
+
+    return FileHandleApi.saveDocument(
+        name: 'agl_advance_invoice.pdf', pdf: pdf);
+  }
+}
